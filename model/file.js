80201c8d8dcd835126ebcb1ce302e7937dd691bb
= <harmostparmod@gmail.com>
2024-05-27 10:57:45 +0530

    added : middleware  nodemailer

--- model/file.js
+++ model/file.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporterInstance = require("../config/mailTransporter");
 
 const fileSchema = new mongoose.Schema({
   name: {
@@ -14,6 +18,23 @@ const fileSchema = new mongoose.Schema({
   imageUrl: {
     type: String,
   },
+});
+
+// post middleware : nodemailer
+fileSchema.post("save", async function (doc) {
+  try {
+    // mail send
+    let info = await transporterInstance.sendMail({
+      from: "Parmod Gang",
+      to: doc.email,
+      subject: "File uploaded to cloudinary",
+      html: `<h1>File Uploaded!!</h1>
+       <p>we r blessed to upload your file on cloud for backups</p>
+        <br/>
+        <a href = "${doc.imageUrl}">View Here</a>
+      `,
+    });
+  } catch (error) {}
 });
 
 const File = mongoose.model("File ", fileSchema);
